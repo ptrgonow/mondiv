c642e9a5dc4c00661724d87cf0bd48af9d32b4cb
Patrick <lusielar@naver.com>
2025-08-10 14:59:15 +0900

    :rocket: feat: 초기 프로젝트 설정
    1. Spring Boot 프로젝트 생성
    - Spring Boot, Gradle 기반의 애플리케이션 구조 생성
    - 주요 Gradle 설정 추가 및 의존성 정의
    
    2. 파일 추가
    - 기본 애플리케이션과 테스트 클래스 추가
    - `.gitignore` 파일 생성 및 설정
    - Gradle Wrapper 관련 스크립트 및 속성 파일 추가
    
    3. 환경 설정
    - 주요 애플리케이션 설정 파일(`application.properties`) 구성

---
+++ src/main/java/com/mondiv/app/MondivApplication.java
@@ -0,0 +1,13 @@
+package com.mondiv.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondivApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MondivApplication.class, args);
+    }
+    
+}
